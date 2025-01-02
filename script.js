433e28a555010ffeb773292310a7913a0076f096
Rukshan Tharindu <97118309+Rukshan2000@users.noreply.github.com>
2025-01-02 21:16:44 +0530

    cover complete

--- script.js
+++ script.js
@@ -1,83 +1,10 @@
-const prev = document.querySelector("#prev");
-const next = document.querySelector("#next");
+// Toggle the mobile menu visibility
+const toggleButton = document.querySelector('[data-collapse-toggle="navbar-sticky"]');
+const navMenu = document.getElementById('navbar-sticky');
 
-let carouselVp = document.querySelector("#carousel-vp");
-
-let cCarouselInner = document.querySelector("#cCarousel-inner");
-let carouselInnerWidth = cCarouselInner.getBoundingClientRect().width;
-
-let leftValue = 0;
-
-// Variable used to set the carousel movement value (card's width + gap)
-const totalMovementSize =
-  parseFloat(
-    document.querySelector(".cCarousel-item").getBoundingClientRect().width,
-    10
-  ) +
-  parseFloat(
-    window.getComputedStyle(cCarouselInner).getPropertyValue("gap"),
-    10
-  );
-
-prev.addEventListener("click", () => {
-  if (!leftValue == 0) {
-    leftValue -= -totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-  }
+// Event listener for the toggle button
+toggleButton.addEventListener('click', () => {
+    const expanded = toggleButton.getAttribute('aria-expanded') === 'true' || false;
+    toggleButton.setAttribute('aria-expanded', !expanded);
+    navMenu.classList.toggle('hidden');
 });
-
-next.addEventListener("click", () => {
-  const carouselVpWidth = carouselVp.getBoundingClientRect().width;
-  if (carouselInnerWidth - Math.abs(leftValue) > carouselVpWidth) {
-    leftValue -= totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-  }
-});
-
-const mediaQuery510 = window.matchMedia("(max-width: 510px)");
-const mediaQuery770 = window.matchMedia("(max-width: 770px)");
-
-mediaQuery510.addEventListener("change", mediaManagement);
-mediaQuery770.addEventListener("change", mediaManagement);
-
-let oldViewportWidth = window.innerWidth;
-
-function mediaManagement() {
-  const newViewportWidth = window.innerWidth;
-
-  if (leftValue <= -totalMovementSize && oldViewportWidth < newViewportWidth) {
-    leftValue += totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-    oldViewportWidth = newViewportWidth;
-  } else if (
-    leftValue <= -totalMovementSize &&
-    oldViewportWidth > newViewportWidth
-  ) {
-    leftValue -= totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-    oldViewportWidth = newViewportWidth;
-  }
-}
-
-
-$('input').on('change', function() {
-    $('body').toggleClass('blue');
-  });
-
-
-function toggleMenu() {
-    const menu = document.querySelector('.nav-center');
-    menu.classList.toggle('show');
-}
-
-
-document.getElementById('burger-menu').addEventListener('click', function() {
-    const drawer = document.getElementById('drawer');
-    const burger = document.getElementById('burger-menu');
-    
-    drawer.classList.toggle('open');
-    burger.classList.toggle('open');
-});
-
-
-
